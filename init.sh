c1b619c95470f3033d7a18e0866b3624259bc701
leonid-invalid <kompanchenko.leonid@gmail.com>
2019-12-08 03:06:18 +0400

    1

--- init.sh
+++ init.sh
@@ -6,8 +6,6 @@ sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo python3 -m pip install gunicorn
 sudo python3 -m pip install django==2.0
 sudo python3 -m pip install mysqlclient
-sudo rm /etc/nginx/sites-available/default
-sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "create database stepik;"

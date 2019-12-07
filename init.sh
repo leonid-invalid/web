65266c696dd8d2fd0a8c09f8dd63a2ef0f8645cf
leonid-invalid <kompanchenko.leonid@gmail.com>
2019-12-08 03:41:47 +0400

    1

--- init.sh
+++ init.sh
@@ -6,7 +6,6 @@ sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo python3 -m pip install gunicorn
 sudo python3 -m pip install django==2.0
 sudo python3 -m pip install mysqlclient
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "create database stepik;"
 sudo mysql -uroot -e "CREATE USER 'leonid'@'localhost' IDENTIFIED BY 'lomtik5455'";
@@ -14,7 +13,6 @@ sudo mysql -uroot -e "grant all privileges on stepik.* to 'leonid'@'localhost' w
 python3 ~/web/ask/manage.py makemigrations
 python3 ~/web/ask/manage.py migrate
 sudo rm /etc/nginx/sites-available/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 cd ~/web/ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application

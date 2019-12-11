02950b55a353708d202ae8c80535891fd80a7301
leonid-invalid <kompanchenko.leonid@gmail.com>
2019-12-12 00:25:56 +0400

    authorization+login

--- ask/accounts/views.py
+++ ask/accounts/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from accounts.forms import UserCreationForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
 
 def SignUp(request):
 	error = ''
-	#question = get_object_or_404(Question, id=id)
-	#answers = Answer.objects.filter(question=question)
 	if request.method == "POST":
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
-			form.clean()
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(request, username=username, password=password)
+			login(request, user)
 			form.save()
-			url = '/login/'
+			url = '/'
 			return HttpResponseRedirect(url)
 		else:
 			error = u'Invalid input'
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views
 from django.urls import path, include
 from qa.views import test, question_new, question_popular, ask
 
 urlpatterns = [
     path('', question_new, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
     path('signup/', include('accounts.urls')),
     path('question/', include('qa.urls')),
     path('ask/', ask, name='ask'),

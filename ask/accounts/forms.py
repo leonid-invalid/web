b16005bbd848e036b9dd94b2ada9491a4eb2af0e
leonid-invalid <kompanchenko.leonid@gmail.com>
2019-12-12 01:03:02 +0400

    authorization+

--- ask/accounts/forms.py
+++ ask/accounts/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserCreationForm(forms.Form):
 	username = forms.CharField(max_length=100)
 	email = forms.EmailField()
-	password = forms.CharField(max_length=100)
+	password = forms.CharField()
 
 	def clean(self):
 		return self.cleaned_data 
--- ask/accounts/views.py
+++ ask/accounts/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from accounts.forms import UserCreationForm
 from django.http import HttpResponseRedirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 def SignUp(request):
 	error = ''
 	if request.method == "POST":
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
-			username = request.POST['username']
-			password = request.POST['password']
-			user = authenticate(request, username=username, password=password)
-			login(request, user)
-			form.save()
-			url = '/'
-			return HttpResponseRedirect(url)
+			try:
+				form.save()
+				user = form.save()
+				login(request, user)
+				url = '/'
+				return HttpResponseRedirect(url)
+			except IntegrityError:
+				error = u'Duplicate username'
 		else:
 			error = u'Invalid input'
 	else:
 		form = UserCreationForm()
 	return render(request, 'signup.html', {
 		'form': form,
+		'error': error,
 	})
